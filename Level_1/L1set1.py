dc7c31982b2d7828be30eb9d85a1c5faed78d46f
Biraja Prasad Routray <birajaroutraywork@gmail.com>
2025-05-05 19:55:18 +0530

    Add files via upload

---
+++ Level_1/L1set1.py
@@ -0,0 +1,68 @@
+
+# 1. Create a variable to store your name and print it.
+name = "willow"
+print(name)
+
+# Store your age in a variable and print “You are X years old”.
+age = 25
+print("You are", age, "years old")
+
+
+# Take user input for two numbers and print their sum.
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+sum = num1 + num2
+print("Sum : ", sum)
+
+
+# Check the data type of True, 3.14, and "Python"
+print(type(True))
+print(type(3.4))
+print(type("python"))
+
+# Swap the values of two variables.
+a = int(input("EAN: "))
+b = int(input("EAN: "))
+
+temp = a
+a = b
+b = temp
+
+print("a: ",a ,"b: ",b)
+
+
+# Create variables of all primitive data types and print them.
+int_num = 10
+float_num = 1.5
+string_text = "Hello"
+boolean_value = True
+
+print("Integer:", int_num)
+print("Float:", float_num)
+print("String:", string_text)
+print("Boolean:", boolean_value)
+
+# Take input for name and age and print: “Hi [name], you are [age] years old.”
+name = input("EAN: ")
+age = int(input("EYA: "))
+print("Hi", name , "You are ", age, "years old")
+
+
+# Multiply a number by 10 and print the result.
+number = int(input("EAN :" ))
+result = number * 10
+print(number, " * 10 = ", result)
+
+
+# Convert a float number to int and vice versa.
+float_num = 5.5
+int_num = int(float_num)
+print("Integer: ", int_num)
+
+int_value = 5
+float_value = float(int_value)
+print("float: ", float_value)
+
+# Print the output of: 5 + 3 * 2 - 8 / 4
+result = 5 + 3 * 2 - 8 / 4
+print("Result:", result)
